9eed229269bafcb1feb0e1cdb1a9c504166ac445
Robin H. <robin@odenwaldschaefer.de>
2016-09-16 23:50:06 +0200

    1. Kommentare hinzugefügt ^^

--- danmashiRPG/src/danmashiRPG/LivingThing.java
+++ danmashiRPG/src/danmashiRPG/LivingThing.java
@@ -10,6 +10,7 @@ public class LivingThing
 	private int Strength;		// Stärke Attribut
 	private int Agility;		// Agilitäts Attribut
 	private int HitPoints;		// Lebenspunkte
+	private int HitPointsMax;	// Maximal-Lebenspunkte
 	private int BaseDamage;		// Standartschaden
 	
 	
@@ -19,12 +20,15 @@ public class LivingThing
 		Name = name;
 		Strength = str;
 		Agility = agi;
-		HitPoints = hp;
+		HitPointsMax = hp;
 		BaseDamage = bd;
+		HitPoints = hp;
 	}
 	
 	// Methoden
-	public int useAttribute(int index)
+	
+	
+	public int useAttribute(int index)	// Standartmethode für Proben -> Rückgabe = Erfolgsgrade / -1 -> Fehlschlag
 	{
 		int result = UsefullStuff.throwDice(100, 1);
 		int successRate = 0;
@@ -55,10 +59,61 @@ public class LivingThing
 		return successRate;	
 	}
 	
+	/* 	verwendet, für schaden und heilung / rückgabewert true -> ziel lebt noch | false -> ziel ist tot
+	 	baseDamage des Angreifers, wenn negativ -> Heilung | bei Heilung successes = 0 annehmen 
+	 * 	und gesammte Heilung als negativer bD  
+	 * 	wenn das leben verändert wird, wird lebensanzeige benutzt
+	 */
+	public boolean changeHealth(int bD , int success) 
+	{
+		HitPoints -= bD + success;
+		
+		// kein überheilen
+		if(HitPoints > HitPointsMax)
+			HitPoints = HitPointsMax;
+		
+		healthBar();
+		
+		return (HitPoints > 0);
+	}
 	
 	
+	/*	Dodge methode, verwendet wenn einem angriff ausgewichen werden soll
+	 * 	success = erfolge des Angreifers, 
+	 * Rückgabewert: true -> ausgewichen -> kein Schaden
+	 * false -> schaden
+	 */
+	public boolean dodge(int success, int bD)
+	{
+		int result = useAttribute(2);
+		// ich weiß nicht ob man den schaden direkt mit ins dodge tut, könnte man aber machen
+		if (result < success)
+			changeHealth(bD, success);
+		
+		return (result >= success);
+	}
 	
 	
+	// Show Methoden
+	
+	//  Show all stats: ausgabe der Stats des dings in der Console
+	public void showStats()
+	{
+		System.out.println(" Name: " + Name);
+		System.out.println(" Lebenskraft: " + Integer.toString(HitPoints) + " / " + Integer.toString(HitPointsMax));
+		System.out.println(" Grundschaden: " + Integer.toString(BaseDamage));
+		System.out.println(" Str: " + Integer.toString(Strength) + "    Agi: " + Integer.toString(Agility));
+		System.out.println();
+	}
+
+	
+	//  Show health: gibt lebensanzeige in der Console aus: mit namen dazu
+	public void healthBar()
+	{
+		System.out.println(" Name: " + Name);
+		System.out.println(" Lebenskraft: " + Integer.toString(HitPoints) + " / " + Integer.toString(HitPointsMax));
+		System.out.println();
+	}
 	
 	
 	
@@ -66,6 +121,11 @@ public class LivingThing
 	
 	
 	// set Methoden mit Kontrolle
+	public void setName( String name)
+	{
+		Name = name;
+	}
+	
 	public void setStrength( int str)
 	{
 		if(str > 0 && str < 1000)
@@ -78,10 +138,10 @@ public class LivingThing
 			Agility = agi;
 	}
 	
-	public void setHitPoints( int hp)
+	public void setHitPointsMax( int hp)
 	{
 		if(hp > 0 && hp < 1000)
-			HitPoints = hp;
+			HitPointsMax = hp;
 	}
 	
 	public void setBaseDamage( int bd)
@@ -92,6 +152,11 @@ public class LivingThing
 
 	
 	// get methoden
+	public String getName()
+	{
+		return Name;
+	}
+	
 	public int getStrength()
 	{
 		return Strength;
@@ -100,6 +165,11 @@ public class LivingThing
 	public int getAgility()
 	{
 		return Agility;
+	}
+	
+	public int getHitPointsMax()
+	{
+		return HitPointsMax;
 	}
 	
 	public int getHitPoints()
--- danmashiRPG/src/danmashiRPG/mainClass.java
+++ danmashiRPG/src/danmashiRPG/mainClass.java
@@ -5,6 +5,19 @@ public class mainClass
 	public static void main(String args[]) 
 	{
 		System.out.println("Willkommen zum danmachi RPG");
+		
+		// erstes testobject
+		LivingThing t1 = new LivingThing("Ding", 25, 65, 20, 3);
+		
+		t1.showStats();
+		
+		t1.changeHealth(5, 2);
+		t1.changeHealth(-6, 0);
+		
+		if(t1.dodge(1, 5))
+			System.out.println(" Ausgewichen ");
+		
+		
 	}
 
 }
